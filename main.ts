a63c684b0a0969cbd3877f99fcb2fcc5699784a2
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-05 11:49:38 -1000

    boom

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 	
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    game.reset()
+    info.setLife(3)
+    info.setScore(0)
 })
-music.play(music.stringPlayable("C C5 B C5 B A - - ", 120), music.PlaybackMode.UntilDone)
+info.onLifeZero(function () {
+    info.changeLifeBy(-3)
+})
+music.play(music.createSong(hex`0078000408020100001c00010a006400f40164000004000000000000000000000000000500000430000400080001290c00100001251400180001221c002000011e24002800011e2c003000012734003800012c38003c000125`), music.PlaybackMode.UntilDone)
+music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
